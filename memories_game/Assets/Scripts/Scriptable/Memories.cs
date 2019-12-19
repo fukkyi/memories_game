ae6f9b3f21c7a2f9e3ebae7a17710e36ff023841
fukkyi <52132729+fukkyi@users.noreply.github.com>
2019-12-20 00:05:43 +0900

    物理演算変更 & リソース準備

---
+++ memories_game/Assets/Scripts/Scriptable/Memories.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/Memories", fileName = "Memories")]
+public class Memories : ScriptableObject
+{
+    public GameObject gameObj;
+
+    public string memoryName;
+
+    [TextArea]
+    public string description;
+}
